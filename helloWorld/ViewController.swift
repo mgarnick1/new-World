75e143cafa6e158d8e35eb821aad1cdbd5b14fd6
Shoenick <Shoenick@shoenicks-mbp.local.tld>
2016-02-29 19:58:10 -0700

    Initial Commit

---
+++ helloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloWorld
+//
+//  Created by Shoenick on 2/29/16.
+//  Copyright © 2016 Shoenick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
